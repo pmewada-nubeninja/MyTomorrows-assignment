b2b4d8fff733d3cc32ba9452066adf544a0048b8
Parth Mewada <parthm2812@gmail.com>
2025-12-10 12:17:26 +0100

    Refactoring Helmchart

--- app/app.py
+++ app/app.py
@@ -32,7 +32,10 @@ def config():
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({'status': 'healthy'}), 200
+    return jsonify({
+        'status': 'healthy',
+        'DB Connection': 'successful' if os.getenv("DB_PASSWORD") else 'not configured'
+    }), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9090)
